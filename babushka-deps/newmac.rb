2a1fb2b7ba225673a568e7c3c6c190b0e471a40c
Gabriel Gilder <gabriel@squareup.com>
2014-09-12 00:59:13 -0700

    brew cask

--- babushka-deps/newmac.rb
+++ babushka-deps/newmac.rb
@@ -2,6 +2,12 @@ dep 'newmac' do
   requires(
     %w(
       homebrew
+      brew-cask
     )
   )
+end
+
+dep 'brew-cask' do
+  met? { `brew tap`.include?('caskroom/cask') }
+  meet { `brew install caskroom/cask/brew-cask` }
 end
