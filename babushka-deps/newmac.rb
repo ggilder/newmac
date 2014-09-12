6eccf442fdee57cb42a3fca4b27f5fe7ae2a6c40
Gabriel Gilder <gabriel@squareup.com>
2014-09-12 01:26:03 -0700

    Add fonts

--- babushka-deps/newmac.rb
+++ babushka-deps/newmac.rb
@@ -3,18 +3,23 @@ CASK_APPS = %w(
   dropbox
   onepassword
   iterm2
-
+  font-meslo-lg-for-powerline
 )
 
 dep 'newmac' do
   requires(
-    %w(homebrew brew-cask) + CASK_APPS.map { |app| "#{app}.brewcask" }
+    %w(homebrew brew-cask brew-fonts) + CASK_APPS.map { |app| "#{app}.brewcask" }
   )
 end
 
 dep 'brew-cask' do
   met? { `brew tap`.include?('caskroom/cask') }
   meet { `brew install caskroom/cask/brew-cask` }
+end
+
+dep 'brew-fonts' do
+  met? { `brew tap`.include?('caskroom/fonts') }
+  meet { `brew tap caskroom/fonts` }
 end
 
 meta :brewcask do
